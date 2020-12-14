f3f4480305730c4b3e5902c1030ef224c059c135
para <adello13@yahoo.fr>
2020-12-14 16:15:50 +0100

    First commit

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
     dbUrl: 'mongodb+srv://para:qwe@dymaformation.9tth3.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: path.join(__dirname, ''),
-    key: path.join(__dirname, '')
+    cert: '/etc/letsencrypt/live/www.underlink.fr/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.underlink.fr/privkey.pem'
 }

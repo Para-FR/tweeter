9be00543846fd2dc886945790c2405ace0434c41
para <adello13@yahoo.fr>
2020-12-16 16:19:54 +0100

    Fix 6 + Update

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -8,7 +8,8 @@ const userSchema = schema({
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true }
     },
-    avatar: { type: String, default: '/images/user.svg' }
+    avatar: { type: String, default: '/images/user.svg' },
+    following: { type: [schema.Types.ObjectId], ref: 'user' }
 });
 
 userSchema.statics.hashPassword = (password) => {
